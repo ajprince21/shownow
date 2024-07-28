4bc4b633a150fe97bf985696ef26e63b43b05270
Ajay Raj Tiwari <ajay.tiwari@sociolla.com>
2024-07-29 01:08:40 +0530

    created nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,5 @@
+import { Badge } from "@material-ui/core";
+import { Search, ShoppingCartOutlined } from "@material-ui/icons";
 import React from "react";
 import styled from "styled-components";
 
@@ -9,19 +11,48 @@ const Wrapper = styled.div`
   padding: 10px 20px;
   display: flex;
   justify-content: space-between;
+  align-items: center;
 `;
 
 const Left = styled.div`
   flex: 1;
+  display: flex;
+  align-items: center;
 `;
 const Center = styled.div`
   flex: 1;
+  text-align: center;
 `;
 const Right = styled.div`
   flex: 1;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
 `;
 const Language = styled.span`
   font-size: 14px;
+  cursor: pointer;
+`;
+
+const Input = styled.input`
+  border: none;
+`;
+
+const Logo = styled.h1`
+  font-weight: 700;
+`;
+
+const SearchBarContainer = styled.div`
+  border: 0.5px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
+
+const MenuItem = styled.div`
+  font-size: 14px;
+  margin-left: 25px;
   cursor: pointer;
 `;
 
@@ -31,9 +62,28 @@ const Navbar = () => {
       <Wrapper>
         <Left>
           <Language>EN</Language>
+          <SearchBarContainer>
+            <Input />
+            <Search
+              style={{
+                color: "gray",
+                fontSize: "16px",
+              }}
+            />
+          </SearchBarContainer>
         </Left>
-        <Center>center</Center>
-        <Right>Right</Right>
+        <Center>
+          <Logo>Show Now</Logo>
+        </Center>
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlined></ShoppingCartOutlined>
+            </Badge>
+          </MenuItem>
+        </Right>
       </Wrapper>
     </Container>
   );
